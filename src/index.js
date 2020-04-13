e40472eee17ce7a8cb39cb4d9f9eedcae77761da
paulivonne <paulivonne@MacBook-Pro-de-Paulina.local>
2020-04-13 10:15:22 -0400

    actualizacion codigo

--- src/index.js
+++ src/index.js
@@ -1,75 +1,71 @@
-import validator from './validator.js';
+//importa constante validador desde validator.js
+import validador from './validator.js';
 
+//prueba numero tarjeta valido 4137894711755904
+//Pantalla1
 document.getElementById("pantalla2").style.display="none";
-document.getElementById("pantalla3").style.display="none";
-document.getElementById("pantalla4").style.display="none";
-
+document.getElementById("cuadro3").style.display="none";
+document.getElementById("cuadro4").style.display="none";
+//Botón1 lleva a pantalla2
 document.getElementById("boton1").addEventListener("click", ()=>{
-    
-    document.getElementById("pantalla2").style.display="block";
-    document.getElementById("pantalla1").style.display="none";
-    document.getElementById("pantalla3").style.display="none";
-    document.getElementById("pantalla4").style.display="none";
-    
+  document.getElementById("pantalla2").style.display="block";
+  document.getElementById("pantalla1").style.display="none";
+  document.getElementById("pantalla1").style.display="none";
 });
-
-document.getElementById("boton2").addEventListener("click", ()=>{
-    
-    document.getElementById("pantalla3").style.display="block";
-    document.getElementById("pantalla1").style.display="none";
-    document.getElementById("pantalla2").style.display="none";
-    document.getElementById("pantalla4").style.display="none";
-    
-});
-
+//Botón3 volver al formulario - invalido
 document.getElementById("boton3").addEventListener("click", ()=>{
-//(funcionvalido)
-    //funcionvalido
-    document.getElementById("pantalla4").style.display="block";
-    document.getElementById("pantalla1").style.display="none";
-    document.getElementById("pantalla2").style.display="none";
-    document.getElementById("pantalla3").style.display="none";
-    
+  document.getElementById("cuadro2").style.display="block";
+  document.getElementById("cuadro3").style.display="none";
+  document.getElementById("cuadro4").style.display="none";   
 });
-
+//Botón4 volver a inicio - valido
 document.getElementById("boton4").addEventListener("click", ()=>{
-    
-    document.getElementById("pantalla1").style.display="block";
-    document.getElementById("pantalla2").style.display="none";
-    document.getElementById("pantalla3").style.display="none";
-    document.getElementById("pantalla4").style.display="none";
-    
+  document.getElementById("pantalla1").style.display="block";
+  document.getElementById("pantalla2").style.display="none";
 });
-//desde aqui se arruina todo xd
-
- const numTarjeta = document.getElementById("creditCardNumber").value;
-
-      let estadoTarjeta = true;
-      let estadoTarjeta == false;
-      let numOculto;
-
-      estadoTarjeta = validator.isValid(numTarjeta);
-
-      if (numTarjeta.length == 0 ) {
-        alert("Debes llenar el campo");
-        document.getElementById("pantalla3").style.display="none";
-        document.getElementById("pantalla2").style.display="block";
-      }
-
-      else{
-
-        if (estadoTarjeta == false) {
-        numOculto= validator.maskify(numTarjeta);
-        
-        document.getElementById("pantalla2").style.display="none";
-        document.getElementById("pantalla4").style.display="block";
-        }
-
-      else (estadoTarjeta == true) {
-            numOculto= validator.maskify(numTarjeta);
-            document.getElementById("pantalla2").style.display="none";
-            document.getElementById("pantalla3").style.display="block";
-        }
+//AQUI PARTEN LAS OPERACIONES
+//Botón2 lleva a: dependiendo de si el valido o no el numero ingresado
+document.getElementById("boton2").addEventListener("click", ()=>{
+  document.getElementById("cuadro3").style.display="none";
+  document.getElementById("cuadro4").style.display="none";
+  document.getElementById("pantalla1").style.display="none";
+//Se declara una constante (numIngresado) que llama al número que se ingresa en numTarjeta con un valor
+  const numIngresado = document.getElementById("numTarjeta").value;
+//Se declarar variable para numResultado valido (resultado)
+  let numResultado = true;
+//Se declarar variable para ocultar numero con # (numOculto)
+  let numOculto;
+//La variable "numResultado" se conecta con el validator(is.Valid)(ya que contiene la formula) que se aplicara a la constante numIngresado
+//"validador" son las dos operaciones y el "isValid" es la operacion especifica de validación
+  numResultado = validador.isValid(numIngresado);
+//SI: todos los numeros dan valor 0 (osea que no se han ingresado numeros)
+    if (numIngresado.length == 0) {
+//Se envia mensaje de alerta y no permite continuar a la otra pagina
+        contenedorVacio.classList.remove('contenedorOculto');
+        contenedorAlerta.classList.add('contenedorOculto');
+          document.getElementById("cuadro2").style.display="block";
+          document.getElementById("cuadro3").style.display="none";
+          document.getElementById("cuadro4").style.display="none";
     }
-    
+    else{
+//TAMBIEN: si el "numResultado" (de la operacion que se le hace al numero ingresado) es falso 
+//Lleva al cuadro3
+      if (numResultado == false) {
+        numOculto= validador.maskify(numIngresado);
+          document.getElementById("cuadro3").style.display="block"; 
+          document.getElementById("cuadro2").style.display="none"; 
+          document.getElementById("numMaskify").innerHTML = numOculto;
+      }
+//No declara nuevamente true por que esta declarado arriba (linea 35)
+      else {
+//TAMBIEN: el numOculto sera el resultado del numIngresado con el validator y el enmascarado aplicados
+//"validador" son las dos operaciones y el "maskify" es la operacion especifica
+        numOculto= validador.maskify(numIngresado);
+//Como es true lleva al cuadro4
+          document.getElementById("cuadro4").style.display="block";
+          document.getElementById("cuadro2").style.display="none";
+//Cambia numMaskify1 (que es el numero ingresado) por numOculto (que es el enmascarado)
+          document.getElementById("numMaskify1").innerHTML = numOculto;
+        }
+    } 
 });
--- src/validator.js
+++ src/validator.js
@@ -1,46 +1,58 @@
-const validator = {
-  isValid : (cardNumber)=>{
-      let reverseNum = Array.from(cardNumber).reverse();
+//se declara constante validador, que contiene el ejercicio de validar y enmascarar
+const validador = {
+  //declara "isValid" como "numLuhn", que contiene el algoritmo de Luhn
+  //numTarjeta-> numIngresado-> numResultado (->numLuhn) <3
+    isValid : (numLuhn)=>{
+  //se declara la variable "numReverso" 
+  //se crea un nuevo array a partir de numLuhn y revierte los numeros
+      let numReverso = Array.from(numLuhn).reverse();
+  //"suma" declara que la suma de todos los numeros ingresados comiencen desde 0 
       let suma = 0;
+  //se declara que el estado natural del ejercicio es verdadero
       let estado = true; 
-
-      
-      for (let i = 0; i < reverseNum.length; i++) 
-      {
-      
-        if (i%2 == 0) { /*numeros pares*/
-            reverseNum[i] = reverseNum[i] * 2;
-            if (reverseNum[i] >= 10) {
-            reverseNum[i] = (reverseNum[i] - 10)+1;
-            }
-            else
-         {
-            n = numeros[i]-'0';
+  //variable i empieza desde 0; condicional: hasta donde ocurre el bucle (for); incrementa 1 (i++)para que el bucle se aplique en todas los numeros ingresados
+      for (let i = 0; i < numReverso.length; i++) {      
+  //con el residuo de 2 se descubre si es una posición par
+  //a los numeros ubicados en una posición par se le aplica residuo y si da cero se multiplica x2
+  //esto define posición
+        if ((i + 1) % 2 == 0) { 
+  //solo si la variable [i] esta en una posición par se multiplica x2
+          numReverso[i] = numReverso[i] * 2; 
+  //Si la multiplicación de la suma es un numero mayor a 9 hay que restarle 9
+          if (numReverso[i] > 9) { 
+          numReverso[i] = numReverso[i] - 9;
           }
-          suma = suma + n;
-         }
         }
-        
-      if (suma % 10 == 0 ){
-        estado == true; 
+  //suma numeros pares e impares y sin el parseInt aparece como NaN 
+        suma += parseInt(numReverso[i]);
       }
-      else {
-        estado = false; 
-      }    
-      
-      return estado;
-      },
-
-  maskify : (cardNumber)=>{
-      let card = Array.from(cardNumber);
-        
-        for (let i = 0; i <=card.length; i++) {
-            if (i<card.length-4){
-                card[i]="#";
-            }
-    
+  //si la suma de todos los numeros con el residuo da 0, es verdadero
+        if (suma % 10 == 0 ){
+          estado == true; 
         }
-
-};
-
-export default validator;
+  //si la suma de todos los numeros no da residuo 0, es falso
+        else {
+          estado = false; 
+        }         
+  //devuelve la respuesta de si el estado es verdadero o falso      
+        return estado;
+        },
+  //declara "maskify" como "numLuhn", que contiene el enmarcarado
+    maskify : (numLuhn)=>{
+  //se declara la variable "numMask" 
+  //crea un nuevo array a partir de numLuhn y enmascara los números
+      let numMask = Array.from(numLuhn);
+  //variable i empieza desde 0; condicional: hasta donde ocurre el bucle (for)(en este caso hasta la longitud del numero (16); incrementa 1 (i++) para que el bucle se aplique en todos los numeros ingresados       
+      for (let i = 0; i <=numMask.length; i++) {
+  //se enmascara hasta la longitud del numero (en este caso 16) restandole 4 (de derecha a izquierda)
+  //independientemente de la cantidad de numeros que haya
+        if (i<numMask.length-4){
+          numMask[i]="#";
+        }
+      }
+  //devuelve el numero enmascarado junto 
+        return numMask.join("");
+    }
+  };
+  //linkea la constante validator al index.js (desde linea 50 validator.js a linea 2 index.js)
+  export default validador;
