e9c31e4f46603944384ad75a1890eb0c9efa69b2
paulivonne <paulivonne@MacBook-Pro-de-Paulina.local>
2020-04-14 17:29:40 -0400

    Actualizacion final

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ document.getElementById("boton1").addEventListener("click", ()=>{
 document.getElementById("boton3").addEventListener("click", ()=>{
   document.getElementById("cuadro2").style.display="block";
   document.getElementById("cuadro3").style.display="none";
-  document.getElementById("cuadro4").style.display="none";   
+  document.getElementById("cuadro4").style.display="none";
+  document.getElementById("contenedorVacio").style.display="none";   
 });
 //Botón4 volver a inicio - valido
 document.getElementById("boton4").addEventListener("click", ()=>{
@@ -22,28 +23,25 @@ document.getElementById("boton4").addEventListener("click", ()=>{
   document.getElementById("cuadro3").style.display="none";
   document.getElementById("cuadro4").style.display="none";
   document.getElementById("cuadro2").style.display="block";
+  document.getElementById("contenedorVacio").style.display="none";
 });
-
 //Botón2 lleva a: dependiendo de si el valido o no el numero ingresado
 document.getElementById("boton2").addEventListener("click", ()=>{
   document.getElementById("cuadro3").style.display="none";
   document.getElementById("cuadro4").style.display="none";
   document.getElementById("pantalla1").style.display="none";
+  document.getElementById("contenedorVacio").style.display="block";
 
+  let contenedorVacio
   const numIngresado = document.getElementById("numTarjeta").value;
   let numResultado = true;
   let numOculto;
-  let contenedorVacio;
-  let contenedorAlerta;
   numResultado = validador.isValid(numIngresado);
-
     if (numIngresado.length == 0) {
         contenedorVacio.classList.remove('contenedorOculto');
-        contenedorAlerta.classList.add('contenedorOculto');
           document.getElementById("cuadro2").style.display="block";
           document.getElementById("cuadro3").style.display="none";
-          document.getElementById("cuadro4").style.display="none";
-          
+          document.getElementById("cuadro4").style.display="none";    
     }
     else{
       if (numResultado == false) {
@@ -61,5 +59,10 @@ document.getElementById("boton2").addEventListener("click", ()=>{
           document.getElementById("numMaskify1").innerHTML = numOculto;
         }
     } 
- 
+});
+document.getElementById("nombres").value;
+document.getElementById("email").value;
+document.getElementById("boton2").addEventListener("click", ()=>{
+  document.getElementById("nombres").value = "";
+  document.getElementById("email").value = "";
 });
